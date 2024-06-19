036c7b207139b006ba45466d0cb715e835481614
Yeam <yeamlakneheshetu9@gmail.com>
2024-06-19 14:21:50 +0300

    home page edited

--- HomePage/home.js
+++ HomePage/home.js
@@ -1,1 +1,26 @@
-console.log("Hi")
+// let home = document.getElementById('one');
+// let about = document.getElementById('two');
+// let service = document.getElementById('three');
+// let garage = document.getElementById('four');
+// let contact = document.getElementById('five');
+
+// home.onmouseover = changeColor;
+// about.onmouseover = changeColor;
+// service.onmouseover = changeColor;
+// garage.onmouseover = changeColor;
+// contact.onmouseover = changeColor;
+
+// contact.onmouseout = resetColor;
+// service.onmouseout = resetColor;
+// about.onmouseout = resetColor;
+// garage.onmouseout = resetColor;
+// home.onmouseout = resetColor;
+
+// function changeColor() {
+//     if (document.getElementById = "two") {}
+//     home.style.backgroundColor = "white";
+// }
+
+// function resetColor() {
+//     home.style.backgroundColor = 'inherit';
+// }
